139910f52f0c0252350b140a4b6e572b18b43ba0
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-04-25 15:40:20 +0700

    menginputkan NIM dan Nama, dan menambahkannya ke node baru dalam linked list

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -13,6 +13,12 @@ void addNode(){
     int nim;
     string nama;
     Node* nodeBaru = new Node();
+    cout << "Masukkan NIM : ";
+	cin >> nim;
+	cout << "Masukkan Nama : ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
 
 }
 
